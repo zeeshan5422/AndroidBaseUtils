a518915d7bdc88bdfe4898b5b3020dc0b6b35995
zeeshan5422 <zeeshan.akhtar@netsoltech.com>
2019-07-30 12:17:14 +0500

    first commit includin base activity, baseFragment, loading Diloag

---
+++ app/src/main/java/com/zues/mykotlinutils/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.zues.mykotlinutils
+
+import android.os.Bundle
+import android.os.Handler
+import com.raza.android.videocompressor.activities.BaseActivity
+import com.zues.islamic.views.LoadingDialog
+
+class MainActivity: BaseActivity() {
+    override val layoutId: Int
+        get() = R.layout.activity_main
+
+    override fun initViews(savedInstanceState: Bundle?) {
+        super.initViews(savedInstanceState)
+
+        LoadingDialog(this).show()
+    }
+}
